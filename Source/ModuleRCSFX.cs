369d3cd880ce729476b0e0d1431e9a639a11f62e
NathanKell <nathankell@gmail.com>
2014-08-25 20:15:11 -0400

    Add EFFECTS back in, clamp thrust to thrusterPower

--- Source/ModuleRCSFX.cs
+++ Source/ModuleRCSFX.cs
@@ -5,7 +5,18 @@ using KSP;
 
 public class ModuleRCSFX : ModuleRCS
 {
-    
+    [KSPField]
+    public bool fullThrust = false; // always use full thrust
+
+    [KSPField()]
+    string runningEffectName = "";
+    [KSPField()]
+    string engageEffectName = "";
+    [KSPField()]
+    string flameoutEffectName = "";
+
+    public bool rcs_active;
+
     [KSPField()]
     public bool useZaxis = false;
     [KSPField]
@@ -98,6 +109,19 @@ public class ModuleRCSFX : ModuleRCS
         thrustForces.Clear();
         if (isEnabled && part.isControllable)
         {
+            if (vessel.ActionGroups[KSPActionGroup.RCS] != rcs_active)
+            {
+                rcs_active = vessel.ActionGroups[KSPActionGroup.RCS];
+                if(!(engageEffectName.Equals("")))
+                    part.Effect(engageEffectName, 1.0f);
+                if(!(runningEffectName.Equals("")))
+                    part.Effect(runningEffectName, 0f);
+                foreach (FXGroup fx in thrusterFX)
+                {
+                    fx.setActive(false);
+                    fx.Power = 0f;
+                }
+            }
             if (vessel.ActionGroups[KSPActionGroup.RCS])
             {
                 Vector3 CoM = vessel.CoM + vessel.rb_velocity * Time.deltaTime;
@@ -116,6 +140,9 @@ public class ModuleRCSFX : ModuleRCS
                             thruster = thrusterTransforms[i].up;
                         float thrust = Mathf.Max(Vector3.Dot(thruster, torque), 0f);
                         thrust += Mathf.Max(Vector3.Dot(thruster, inputLinear), 0f);
+                        if (thrust > 0 && fullThrust)
+                            thrust = thrusterPower * ( precision ? 0.1f : 1f);
+
                         if (correctThrust)
                             thrust *= realISP / maxIsp;
                         if (thrust > 0.0001f)
@@ -126,8 +153,8 @@ public class ModuleRCSFX : ModuleRCS
                                 if (arm > 1.0f)
                                     thrust = thrust / arm;
                             }
+                            thrust = Mathf.Clamp(thrust, 0f, thrusterPower);
                             UpdatePropellantStatus();
-
                             thrust = CalculateThrust(thrust, out success);
                             thrustForces.Add(thrust);
                             if (success)
@@ -144,9 +171,13 @@ public class ModuleRCSFX : ModuleRCS
                                     effectPower = thrusterFX[i].Power;
                                 thrusterFX[i].setActive(thrust > 0f);
                             }
+                            else if (! (flameoutEffectName.Equals("")))
+                                part.Effect(flameoutEffectName, 1.0f);
                         }
                     }
                 }
+                if(!(runningEffectName.Equals("")))
+                    part.Effect(runningEffectName, effectPower);
             }
         }
         if (!success)
